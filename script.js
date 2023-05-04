7824f76c5e68e7485c2bf7138ff4f5ccb66878e7
EmanuelAyala <24siemprels@gmail.com>
2023-05-03 22:43:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = document.querySelector('.info-title');
+title.addEventListener('mousemove', (e) => {
+  const rect = e.target.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  e.target.style.setProperty('--x', `${ x }px`);
+  e.target.style.setProperty('--y', `${ y }px`);
+});
